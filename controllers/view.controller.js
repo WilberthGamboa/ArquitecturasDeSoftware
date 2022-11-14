ea31e22e97e0619367b21d7fe8b0796e668885d3
wilberth <wgcampos15@gmail.com>
2022-11-13 22:01:57 -0600

    ui post

--- controllers/view.controller.js
+++ controllers/view.controller.js
@@ -1,11 +1,11 @@
-
+const path=require("path")
 const index = (req, res) =>{
-    console.log(__dirname)
-    res.sendFile(__dirname+'/../views/index.html');
+   
+    res.sendFile(__dirname +'/../views/index.html');
 }
 
 const agregar = (req, res) =>{
-    res.sendFile(__dirname+'/../views/agregar.html');
+    res.sendFile(path.join(__dirname + '/../views/agregar.html'));
 }
 
 
---
+++ public/scripts/form.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    
+    const btnSubmit = document.querySelector('#btnSubmit');
+    
+
+   
+    btnSubmit.addEventListener('click', function(e)  {
+        e.preventDefault();
+        const btnsInputNombre= document.querySelector('#nombre');
+        const btnsInputNumero= document.querySelector('#numero');
+        console.log(Number(btnsInputNumero.value));
+        console.log(btnsInputNombre.value)
+        const req = {
+            "nombre": btnsInputNombre.value,
+            "numero":btnsInputNumero.value
+        }
+        fetch('http://localhost:3000/api/usuarios',{
+            method:'POST',
+            body: JSON.stringify(req),
+            headers:{
+                "Content-type":"application/json"
+            }
+
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+        
+/*
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+            */
+           
+    })
+
+});
+
+
+
+
+
+
+
