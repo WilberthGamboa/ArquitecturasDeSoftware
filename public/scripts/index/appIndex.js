a8d4fc068d936372077c0ed85e451aaeda30eaa7
wilberth <wgcampos15@gmail.com>
2022-11-23 00:04:25 -0600

    actualizacion paginacion

--- public/scripts/index/appIndex.js
+++ public/scripts/index/appIndex.js
@@ -24,6 +24,8 @@ const domIndex = new DomIndex();
 const btnBuscar = document.querySelector('#buscarNombre');
 const btnSiguiente = document.querySelector('#btnSiguiente');
 const btnAnterior = document.querySelector('#btnAnterior');
+const paginaActual = document.querySelector('#paginaActual');
+paginaActual
 const {
   host, hostname, href, origin, pathname, port, protocol, search
 } = window.location
@@ -33,19 +35,21 @@ btnBuscar.addEventListener('click', (e) =>{
   history.pushState(null,'prueba','/?busqueda='+textoBuscar.value)
   //console.log(search)
   domIndex.listarUsuariosDom('/?busqueda='+textoBuscar.value);
-
-
+  x = 0;
+  paginaActual.innerHTML=1;
 })
 
 let x = 0;
 
 btnSiguiente.addEventListener('click',(e)=>{
   x=x+1;
+  paginaActual.innerHTML=x+1;
   domIndex.listarUsuariosDom('/?busqueda='+textoBuscar.value+"&&desde="+x);
 })
 
 btnAnterior.addEventListener('click',(e)=>{
   x=x-1;
+  paginaActual.innerHTML=x+1;
   domIndex.listarUsuariosDom('/?busqueda='+textoBuscar.value+"&&desde="+x);
 })
 
