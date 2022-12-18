98ad1ac796284c2a05d5957b5208f27c75395f02
Your Name~ <wgcampos15@gmail.com>
2022-12-17 20:14:40 -0600

    agregando env

--- database/config.js
+++ database/config.js
@@ -2,8 +2,8 @@ const {Sequelize} = require('sequelize');
 //const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = require('../config');
 
 
-const db = new Sequelize('simple','root','',{
-   // host:'DB_HOST',
+const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
+    host:'DB_HOST',
     dialect:'mysql',
 
 });
--- models/server.js
+++ models/server.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const db = require('../database/config');
+
 //const { PORT } = require('../config');
 
 
 class Server{
     constructor(){
         this.app = express();
-        this.port=3000;
+        this.port=process.env.PORT;
         this.usuariosPath = '/api/usuarios';
     
         this.conectarDB();
