5f68bbf7e7379fe5a67170d30e4e241c119c90d0
Your Name <you@example.com>
2023-01-15 00:15:26 -0600

    env and config change

--- database/config.js
+++ database/config.js
@@ -2,9 +2,10 @@ const {Sequelize} = require('sequelize');
 //const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = require('../config');
 
 
-const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-    host:'DB_HOST',
+const db = new Sequelize(process.env.MYSQLDATABASE,process.env.MYSQLUSER,process.env.MYSQLPASSWORD,{
+    host:process.env.MYSQLHOST,
     dialect:'mysql',
+    port:process.env.MYSQLPORT
 
 });
 
